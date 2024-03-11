1379907d93a4fcb39d8d23f81fe2beae89727532
blackPanther <herrt@dhcp-wlce193128.zim.uni-duisburg-essen.de>
2024-03-11 15:15:17 +0000

    refa() split project into testing and logic file

--- src/meta_circular_evaluator.jl
+++ src/meta_circular_evaluator.jl
@@ -1,4 +1,6 @@
-debug = true
+include("testing.jl")
+
+debug = false
 
 # function metajulia_repl()
 #     while true
@@ -98,61 +100,6 @@ function eval_block(block_exp, scope)
     end
     # Return the result of the last expression
     return result
-end
-
-function test_project()
-    println("--- START TESTS ---")
-    # test_basic_math_operators()
-    # test_comparison_operators()
-    # test_different_bool_syntax()
-    test_let_syntax()
-    println("--- TESTS PERFORMED ---")
-end
-
-function test_basic_math_operators()
-    println(">>> TEST BASIC MATH OPERATORS >>>")
-    @assert(meta_eval(:1) == 1)
-    @assert(meta_eval(:"Hello, world!") == "Hello, world!")
-    @assert(meta_eval(:(1 + 2)) == 3)
-    @assert(meta_eval(:((1 + 2) + (1 + 2))) == 6)
-    @assert(meta_eval(:((2 + 3) * (4 + 5))) == 45)
-    println("<<< BASIC MATH OPERATORS TESTED <<<")
-end
-
-function test_comparison_operators()
-    println(">>> TEST COMPARISON OPERATORS >>>")
-    @assert(meta_eval(:(3 > 2)) == true)
-    @assert(meta_eval(:(3 < 2)) == false)
-    @assert(meta_eval(:(3 > 2 && 3 < 2)) == false)
-    @assert(meta_eval(:(3 > 2 || 3 < 2)) == true)
-    println("<<< COMPARISON OPERATORS TESTED <<<")
-end
-
-function test_different_bool_syntax()
-    println(">>> TEST DIFFERENT BOOL SYNTAX >>>")
-
-    println("*** TERNARY OPERATOR ***")
-    @assert(meta_eval(:(3 > 2 ? 1 : 0)) == 1)
-    @assert(meta_eval(:(5 > 2 ? 1 : 0)) == 1)
-    @assert(meta_eval(:(5 < 2 ? 1 : 15)) == 15)
-    @assert(meta_eval(:(2 < 6 ? 4 : 1)) == 4)
-  
-    println("*** PYTHON SYNTAX ***")
-    @assert(meta_eval(:( if 3 > 2 1 else 0 end)) == 1)
-    @assert(meta_eval(:( if 5 > 2 1 else 0 end)) == 1)
-    @assert(meta_eval(:( if 5 < 2 1 else 15 end)) == 15)
-    @assert(meta_eval(:( if 2 < 6 4 else 1 end)) == 4)
-
-    println("<<< DIFFERENT BOOL SYNTAX TESTED <<<")
-end
-
-function test_let_syntax()
-    println("*** LET SYNTAX ***")
-    @assert(meta_eval(:(let x = 1; x end)) == 1)
-    @assert(meta_eval(:(let x = 2; x*3 end)) == 6)
-    @assert(meta_eval(:(let a = 1, b = 2; let a = 3; a+b end end)) == 5)
-    @assert(meta_eval(:(let a = 1; a + 2 end)) == 3)
-    println("*** TEST OF LET DONE SYNTAX ***")
 end
 
 test_project()
---
+++ src/testing.jl
@@ -0,0 +1,63 @@
+function test_project()
+    println("--- START TESTS ---")
+    test_basic_math_operators()
+    test_comparison_operators()
+    test_different_bool_syntax()
+    test_let()
+    println("--- TESTS PERFORMED ---")
+end
+
+function test_basic_math_operators()
+    println(">>> TEST BASIC MATH OPERATORS >>>")
+    @assert(meta_eval(:1) == 1)
+    @assert(meta_eval(:"Hello, world!") == "Hello, world!")
+    @assert(meta_eval(:(1 + 2)) == 3)
+    @assert(meta_eval(:((1 + 2) + (1 + 2))) == 6)
+    @assert(meta_eval(:((2 + 3) * (4 + 5))) == 45)
+    println("<<< BASIC MATH OPERATORS TESTED <<<")
+end
+
+function test_comparison_operators()
+    println(">>> TEST COMPARISON OPERATORS >>>")
+    @assert(meta_eval(:(3 > 2)) == true)
+    @assert(meta_eval(:(3 < 2)) == false)
+    @assert(meta_eval(:(3 > 2 && 3 < 2)) == false)
+    @assert(meta_eval(:(3 > 2 || 3 < 2)) == true)
+    println("<<< COMPARISON OPERATORS TESTED <<<")
+end
+
+function test_different_bool_syntax()
+    println(">>> TEST DIFFERENT BOOL SYNTAX >>>")
+
+    println("*** TERNARY OPERATOR ***")
+    @assert(meta_eval(:(3 > 2 ? 1 : 0)) == 1)
+    @assert(meta_eval(:(5 > 2 ? 1 : 0)) == 1)
+    @assert(meta_eval(:(5 < 2 ? 1 : 15)) == 15)
+    @assert(meta_eval(:(2 < 6 ? 4 : 1)) == 4)
+  
+    println("*** PYTHON SYNTAX ***")
+    @assert(meta_eval(:( if 3 > 2 1 else 0 end)) == 1)
+    @assert(meta_eval(:( if 5 > 2 1 else 0 end)) == 1)
+    @assert(meta_eval(:( if 5 < 2 1 else 15 end)) == 15)
+    @assert(meta_eval(:( if 2 < 6 4 else 1 end)) == 4)
+
+    println("<<< DIFFERENT BOOL SYNTAX TESTED <<<")
+end
+
+function test_let()
+    println(">>> TEST LET >>>")
+    @assert(meta_eval(:(let x = 1; x end)) == 1)
+    @assert(meta_eval(:(let x = 2; x*3 end)) == 6)
+    @assert(meta_eval(:(let a = 1, b = 2; let a = 3; a+b end end)) == 5)
+    @assert(meta_eval(:(let a = 1; a + 2 end)) == 3)
+    println(">>> LET TESTED >>>")
+end
+
+function test_let_syntax()
+    println(">>> TEST LET >>>")
+    @assert(meta_eval(:(let x = 1; x end)) == 1)
+    @assert(meta_eval(:(let x = 2; x*3 end)) == 6)
+    @assert(meta_eval(:(let a = 1, b = 2; let a = 3; a+b end end)) == 5)
+    @assert(meta_eval(:(let a = 1; a + 2 end)) == 3)
+    println(">>> LET TESTED >>>")
+end
